590566b57f285891085762065f3949fe396ef6cf
Ramnadh7 <Ramanadh99@gmail.com>
2023-06-08 16:53:55 +0530

    Day 1 Tasksi

---
+++ Exercise2.py
@@ -0,0 +1,11 @@
+"""
+Write the program to print the below lines.
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+
+"""
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
